0e7e29b8e5349b936bf1ba297dba7b31fe0c4b86
Roshan Jossey <roshanjossey@gmail.com>
2018-01-08 21:59:37 +0530

    more debugging

--- api/controllers/botController.js
+++ api/controllers/botController.js
@@ -1,9 +1,10 @@
 const request = require('request');
 
 exports.handleSubmit = function(req, res) {
-  console.log(req.body.payload.action);
-  const pull_request = req.body.payload.pull_request;
-  if(req.body.payload.action === 'opened') {
+  console.log(req.body.action);
+  const pull_request = req.body.pull_request;
+  if(req.body.action === 'opened') {
+    console.log(req.body.pull_request.diff_url);
     console.log(pull_request.diff_url);
     request(pull_request.diff_url, (error, response, body) => {
 
