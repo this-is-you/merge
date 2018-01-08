b07cd3666ebab44dede650b285a832953502d353
Roshan Jossey <roshanjossey@gmail.com>
2018-01-08 23:42:00 +0530

    Fix mergable

--- api/controllers/botController.js
+++ api/controllers/botController.js
@@ -1,35 +1,34 @@
 const request = require('request');
 
 exports.handleSubmit = function(req, res) {
-  console.log(req.body.action);
-  const pull_request = req.body.pull_request;
-  if(req.body.action === 'opened' && pull_request.mergeable === 'true') {
-    console.log(req.body.pull_request.diff_url);
-    console.log(pull_request.diff_url);
-    request(pull_request.diff_url, (error, response, body) => {
+  const pullRequest = req.body.pull_request;
+  if(req.body.action === 'opened' && isSingleAddition(pullRequest)) {
+    request(pullRequest.diff_url, (error, response, body) => {
 
       if(isSingleFileChangeAndNoDeletions(body) && isChangeInContributorsFile(body)) {
         const options = {
-          url: pull_request.issue_url,
-          json: { body: `Hi  @${pull_request.user.login}, I'm working on a similar project to help beginners to get started with contributing to open source projects.
+          url: `${pullRequest.url}/merge`,
+          json: { body: `Hi  @${pullRequest.user.login}, I'm quite elated about your pull request. I wanna evolve this project to addresses various problems faced by first-time contributors. I'd love to learn about your journey in open source community, the problems, pain points you had etc.
+Could you explain how you felt when you went through the tutorial, made a pull request and learned that I merged it?
 
-Do check it out https://github.com/Roshanjossey/first-contributions` },
+ We’ve recently added social share  to our web app. Could you please go to https://roshanjossey.github.io/first-contributions/#social-share and share your first contribution to open source?  Also, check out projects with easy issues while you’re there.` },
           headers: {
             Authorization: process.env.GITHUB_SECRET,
             'User-Agent': 'request',
           },
         };
-
-        request.post(options, function (error, response, body) {
-          console.log('error:', error); // Print the error if one occurred
-          console.log('options:', options); // Print the error if one occurred
-          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        });
-        options.url = `${pull_request.url}/merge`
         request.put(options, function (error, response, body) {
           console.log('error:', error); // Print the error if one occurred
           console.log('options:', options); // Print the error if one occurred
           console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          options.url = pullRequest.issue_url
+          if(response.statusCode === 200) {
+            request.post(options, function (error, response, body) {
+              console.log('error:', error); // Print the error if one occurred
+              console.log('options:', options); // Print the error if one occurred
+              console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            });
+          }
         });
       }
     })
@@ -44,3 +43,7 @@ const isSingleFileChangeAndNoDeletions = function(diff) {
 const isChangeInContributorsFile = function(diff) {
   return diff.match(/Contributors\.md/g).length === 4;
 };
+
+const isSingleAddition = function(pullRequest) {
+  return(pullRequest.additions === 1 && pullRequest.deletions === 0 && pullRequest.changed_files === 1);
+}
