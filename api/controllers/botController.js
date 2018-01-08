5291487e4038e6c775e771170c90cc6160b3cc12
Roshan Jossey <roshanjossey@gmail.com>
2018-01-08 21:43:05 +0530

    more debuggging

--- api/controllers/botController.js
+++ api/controllers/botController.js
@@ -1,10 +1,10 @@
 const request = require('request');
 
 exports.handleSubmit = function(req, res) {
-  console.log(req.body.action);
-  const pull_request = req.body.pull_request;
-  if(req.body.action === 'opened') {
-  console.log(req.body.action.pull_request.diff_url);
+  console.log(req.body.payload.action);
+  const pull_request = req.body.payload.pull_request;
+  if(req.body.payload.action === 'opened') {
+    console.log(pull_request.diff_url);
     request(pull_request.diff_url, (error, response, body) => {
 
       if(isSingleFileChangeAndNoDeletions(body) && isChangeInContributorsFile(body)) {
