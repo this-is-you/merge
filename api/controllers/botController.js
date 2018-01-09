f7b8027481bdede8d2bba7ffa97ed3e3fcb6e8c2
Roshan Jossey <roshanjossey@gmail.com>
2018-01-09 08:11:04 +0530

    Change condition for accepting pull request

--- api/controllers/botController.js
+++ api/controllers/botController.js
@@ -2,10 +2,10 @@ const request = require('request');
 
 exports.handleSubmit = function(req, res) {
   const pullRequest = req.body.pull_request;
-  if(req.body.action === 'opened' && isSingleAddition(pullRequest)) {
+  if(req.body.action === 'opened' && (isSingleAddition(pullRequest) || isTwoAdditionsAndSingleDeletion(pullRequest))) {
     request(pullRequest.diff_url, (error, response, body) => {
 
-      if(isSingleFileChangeAndNoDeletions(body) && isChangeInContributorsFile(body)) {
+      if(isChangeInContributorsFile(body)) {
         const options = {
           url: `${pullRequest.url}/merge`,
           json: { body: `Hi  @${pullRequest.user.login}, I'm quite elated about your pull request. I wanna evolve this project to addresses various problems faced by first-time contributors. I'd love to learn about your journey in open source community, the problems, pain points you had etc.
@@ -36,14 +36,14 @@ Could you explain how you felt when you went through the tutorial, made a pull r
   res.json({message: "Awesome"});
 }
 
-const isSingleFileChangeAndNoDeletions = function(diff) {
-  return (diff.match(/[^-]--[^-]/g) || [] ).length === 1;
-};
-
 const isChangeInContributorsFile = function(diff) {
   return (diff.match(/Contributors\.md/g) || [] ).length === 4;
 };
 
 const isSingleAddition = function(pullRequest) {
   return(pullRequest.additions === 1 && pullRequest.deletions === 0 && pullRequest.changed_files === 1);
+}
+
+const isTwoAdditionsAndSingleDeletion = function(pullRequest) {
+  return(pullRequest.additions === 2 && pullRequest.deletions === 1 && pullRequest.changed_files === 1);
 }
